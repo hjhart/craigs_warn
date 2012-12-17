d94c8ad85da8156f606ebab8f9f29ab91392e8bc
James Hart <hjhart@gmail.com>
2012-12-16 19:36:04 -0800

    Updated which apartments to search for.

--- apt_warner.rb
+++ apt_warner.rb
@@ -8,29 +8,27 @@ require 'prowl_notifier'
 require 'yaml'
 require 'pony'
 
-urls = ["http://sfbay.craigslist.org/search/apa/sfc?query=&srchType=A&minAsk=&maxAsk=2200&bedrooms=&addThree=wooof", "http://sfbay.craigslist.org/search/roo/sfc?query=&srchType=A&minAsk=&maxAsk=2200&bedrooms=&addThree=wooof", "http://sfbay.craigslist.org/search/sub/sfc?query=&srchType=A&minAsk=&maxAsk=2200&bedrooms=&addThree=wooof"]
+urls = ["http://sfbay.craigslist.org/search/apa/sfc?query=&srchType=A&zoomToPosting=&minAsk=2400&maxAsk=3300&bedrooms=2&nh=2&nh=149&nh=4&nh=12&nh=14&nh=10&nh=24&nh=18&nh=23&nh=30"]
 
-urls.each do |url|  
+urls.each do |url|
   listing = CraigScrape::Listings.new url
   current_listings_count = listing.posts.length
   puts "Found %d posts for the search on this page" % current_listings_count
- 
- FileUtils.mkdir_p('search_counts/apartments')
+
+  FileUtils.mkdir_p('search_counts/apartments')
   listing.posts.each do |post|
     posting_url = post.url
     number = posting_url.match(/\d+/)[0]
     puts "The number matched was #{number} on #{posting_url}"
 
-  search_count_file = File.join('search_counts', 'apartments', number)
+    search_count_file = File.join('search_counts', 'apartments', number)
 
-  unless(File.exists? search_count_file)
-    puts "Never seen this one before, marking as unsent."
-    File.open(search_count_file, 'w') { |f| f.puts current_listings_count }
-    last_posting = post
-    event, description = "Found new apartment", "Location: #{last_posting.location}\n#{last_posting.label} #{last_posting.price}"
-    send_email event, description, posting_url
-    File.open(search_count_file, 'w') { |f| f.puts current_listings_count }
-    puts "Sending prowl message: #{event}, #{description}"
+    unless (File.exists? search_count_file)
+      puts "Never seen this one before, marking as unsent."
+      File.open(search_count_file, 'w') { |f| f.puts current_listings_count }
+      event, description = "Found new apartment", "Location: #{post.location}\n#{post.label} #{post.price}"
+      send_email event, description, posting_url
+      puts "Sending email message: #{event}, #{description}"
     end
   end
 end
